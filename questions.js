b7af31d4dd953f484faad4887f4deb5c2ae41907
Tayler Wiley <taybay@Taylers-iMac.local>
2022-04-21 16:55:34 -0500

    added the questions

---
+++ questions.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        type: 'list',
+        name: 'Q1',
+        message: 'What year was the Declaration Of Independence signed?',
+        choices: [
+            "1777",
+            "1774",
+            "1779",
+            "1776",
+        ]
+    },
+    {
+        type: 'list',
+        name: 'Q2',
+        message: 'How many players are on a baseball team?',
+        choices: [
+            "7",
+            "12",
+            "8",
+            "9",
+        ]
+    },
+    {
+        type: 'list',
+        name: 'Q3',
+        message: 'What year did Disney World open?',
+        choices: [
+            "1969",
+            "1974",
+            "1971",
+            "1972",
+        ]
+    },
+    {
+        type: 'list',
+        name: 'Q4',
+        message: 'What is the mascot of the NBA team in New Orleans?',
+        choices: [
+            "Hornets",
+            "Pelicans",
+            "Bobcats",
+            "Warriors",
+        ]
+    },
+    {
+        type: 'list',
+        name: 'Q5',
+        message: 'What is the mascot of the NBA team in New Orleans?',
+        choices: [
+            "Hornets",
+            "Pelicans",
+            "Bobcats",
+            "Warriors",
+        ]
+
+    },
+]
